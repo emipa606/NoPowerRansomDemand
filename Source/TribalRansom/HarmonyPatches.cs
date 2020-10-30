1ac402c0af2e162dc21a827ca51b130ed3b7ef79
emipa606 <emil.offermo@litet.net>
2020-10-30 22:49:16 +0100

    First publish

---
+++ Source/TribalRansom/HarmonyPatches.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using RimWorld;
+using HarmonyLib;
+using Verse;
+
+namespace TribalRansom
+{
+
+    public static class HarmonyPatches
+    {
+
+        public static bool IsCheckingForRansom = false;
+
+        [HarmonyPatch(typeof(IncidentWorker_RansomDemand), "CanFireNowSub", new Type[] { typeof(IncidentParms) })]
+        public static class CanFireNowSub_Patch
+        {
+            public static void Prefix()
+            {
+                IsCheckingForRansom = true;
+            }
+
+            public static void Postfix()
+            {
+                IsCheckingForRansom = false;
+            }
+        }
+
+        [HarmonyPatch(typeof(CommsConsoleUtility), "PlayerHasPoweredCommsConsole", new Type[] { typeof(Map) })]
+        public static class PlayerHasPoweredCommsConsole_Patch
+        {
+            public static void Postfix(Map map, ref bool __result)
+            {
+                if (!__result)
+                {
+                    __result = TribalRansom.PlayerHasPoweredCommsConsole(map, out _);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(LetterStack), "ReceiveLetter", new Type[] { typeof(Letter), typeof(string) })]
+        public static class ReceiveLetter_Patch
+        {
+            public static void Prefix(ref Letter let)
+            {
+                if (!(let is ChoiceLetter_RansomDemand))
+                {
+                    return;
+                }
+                var letter = (ChoiceLetter_RansomDemand)let;
+                if (!TribalRansom.PlayerHasPoweredCommsConsole(letter.map, out ThingDef type) || type == null)
+                {
+                    return;
+                }
+                letter.title = TranslatorFormattedStringExtensions.Translate("TribalDemandTitle", letter.map.Parent.Label);
+                letter.radioMode = false;
+                if (type.defName == "BirdPostMessageTable")
+                {
+                    letter.text = GenText.AdjustedFor(TranslatorFormattedStringExtensions.Translate("TribalDemandPigeon", letter.kidnapped.LabelShort, letter.faction.Name, letter.fee, letter.kidnapped.Named("PAWN")), letter.kidnapped, "PAWN");
+                }
+                if (type.defName == "SignalFire")
+                {
+                    letter.text = GenText.AdjustedFor(TranslatorFormattedStringExtensions.Translate("TribalDemandSignalfire", letter.kidnapped.LabelShort, letter.faction.Name, letter.fee, letter.kidnapped.Named("PAWN")), letter.kidnapped, "PAWN");
+                }
+                let = letter;
+            }
+        }
+    }
+
+}
---
+++ Source/TribalRansom/NoCommsConsoleUtility.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace TribalRansom
+{
+    [StaticConstructorOnStartup]
+    public static class TribalRansom
+    {
+        static TribalRansom()
+        {
+            var harmonyInstance = new Harmony("Mlie.TribalRansom");
+            harmonyInstance.PatchAll();
+        }
+
+        public static bool PlayerHasPoweredCommsConsole(Map map, out ThingDef type)
+        {
+            type = null;
+            if (ModLister.GetActiveModWithIdentifier("sulusdacor.meltup.nopowercommssimplified") != null)
+            {
+                var birdPostDef = DefDatabase<ThingDef>.GetNamedSilentFail("BirdPostMessageTable");
+                var desks = (from Thing desk in map.listerThings.ThingsMatching(ThingRequest.ForDef(birdPostDef)) where desk.Faction != null && desk.Faction == Faction.OfPlayerSilentFail select desk).ToList();
+                foreach (var desk in desks)
+                {
+                    if (desk.TryGetComp<CompPowerTrader>() != null && desk.TryGetComp<CompPowerTrader>().PowerOn)
+                    {
+                        type = birdPostDef;
+                        return true;
+                    }
+                }
+            }
+            if (ModLister.GetActiveModWithIdentifier("mlie.tribalsignalfire") != null)
+            {
+                var signalFireDef = DefDatabase<ThingDef>.GetNamedSilentFail("SignalFire");
+                var fires = (from Thing fire in map.listerThings.ThingsMatching(ThingRequest.ForDef(signalFireDef)) where fire.Faction != null && fire.Faction == Faction.OfPlayerSilentFail select fire).ToList();
+                foreach (var fire in fires)
+                {
+                    if (fire.TryGetComp<CompRefuelable>() != null && fire.TryGetComp<CompRefuelable>().HasFuel)
+                    {
+                        type = signalFireDef;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
